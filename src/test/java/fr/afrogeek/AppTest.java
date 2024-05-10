058f540efe33ef428d1dd86cab8cdcf9428b2ef8
Emmanuella Pougom <emmanuellagisele.pougomtima@studmail.w-hs.de>
2024-05-10 09:34:04 +0100

    2 sumArray update

--- src/test/java/fr/afrogeek/AppTest.java
+++ src/test/java/fr/afrogeek/AppTest.java
@@ -1,10 +1,9 @@
 package fr.afrogeek;
 
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
@@ -15,18 +14,7 @@ public class AppTest {
      * Rigorous Test :-)
      */
     @ Test
-    public void testApp()
-    {
+    public void testApp() {
         assertTrue(true);
-        // Préparation des données d'entrée
-        double[] arr = {1.0, 2.0, 3.0, 4.0, 5.0};
-        int n = arr.length;
-
-        // Appel de la méthode à tester
-        double sum = App.sumArray(arr, n);
-
-        // Vérification du résultat attendu
-        assertEquals(15.0, sum);
-
     }
 }
